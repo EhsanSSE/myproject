ed67c617c12b9193fdff19b37d3ad0d46d6f1837
Ehsan Shirvani <ehsanshirvani33@gmail.com>
2025-01-19 18:39:50 +0330

    added url and function for category, author,searchbar posts
    added url for posts that seprated categories and author and for search any thing in content

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', blog_view, name='index'),
-    path('<int:pk>', blog_details_view, name='details')
-    
+    path('<int:pk>', blog_details_view, name='details'),
+    path('category/<str:category>', blog_view, name='category'),
+    path('author/<str:author>', blog_view, name='author'),
+    path('search/', blog_search_view, name='search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,14 @@ from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
-def blog_view(request):
-    posts = Post.objects.filter(status=True, published_date__lte=now())
+def blog_view(request, category=None, author=None):
+    filters = {'status': True, 'published_date__lte': now()}
+    if category:
+        filters['category__name'] = category
+    elif author:
+        filters['author__first_name'] = author
+
+    posts = Post.objects.filter(**filters)
     context = {'posts': posts}
     return render(request, 'blog/blog-3.html', context)
 
@@ -21,3 +27,11 @@ def blog_details_view(request, pk):
     next_post = posts_list[index_post + 1] if index_post < len(posts_list) -1 else None
     context = {'post': post, 'previous_post': previous_post, 'next_post': next_post}
     return render(request, 'blog/blog-details.html', context)
+
+def blog_search_view(request):
+    posts = Post.objects.filter(status=True, published_date__lte=now())
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-3.html', context)
